e8b347429e9c0a001f705d284da4b3ab95a88ecc
Guilherme Suganame <gsuganame@gmail.com>
2022-10-19 22:17:44 -0300

    feat: start creating navbar

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+
+export const Navbar: React.FC = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  return (
+    <div className="w-full bg-base-100 flex-column box-border min-h-[4rem] items-center p-[0.5rem]">
+      <div className="flex justify-end">
+        <div className="md:hidden block flex-none self-end">
+          <button
+            className="btn btn-square btn-ghost"
+            onClick={() => setShowNavbar(!showNavbar)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <div
+        className={`md:hidden w-full items-center justify-center flex overflow-hidden transition-all duration-500 h-[calc(100vh-4rem)] ${
+          showNavbar ? "opacity-100" : "opacity-0 h-0"
+        }`}
+      >
+        <ul>
+          <li>
+            <a>Inicio</a>
+          </li>
+          <li>
+            <a>Adote</a>
+          </li>
+          <li>
+            <a>Doação</a>
+          </li>
+          <li>
+            <a>Contato</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex justify-end w-screen self-end">
+        <div className="flex-none hidden md:block">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <a>Inicio</a>
+            </li>
+            <li>
+              <a>Adote</a>
+            </li>
+            <li>
+              <a>Doação</a>
+            </li>
+            <li>
+              <a>Contato</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* <div className="flex">
+        <ul>
+          <li>
+            <a>Inicio</a>
+          </li>
+          <li>
+            <a>Adote</a>
+          </li>
+          <li>
+            <a>Doação</a>
+          </li>
+          <li>
+            <a>Contato</a>
+          </li>
+        </ul>
+      </div> */}
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,7 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { Navbar } from "../components/Navbar";
 
 const Home: NextPage = () => {
-  return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal p-0">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <a>
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2 bg-base-100">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+  return <Navbar />;
 };
-
 export default Home;
