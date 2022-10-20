d53f3aa0648c90c5c8379eec5c6f3650319ff385
Guilherme Suganame <gsuganame@gmail.com>
2022-10-20 11:38:08 -0300

    feat: define primary and secondary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      primaryOng: "#102a54",
+      red: "#f51e26",
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "#102a54",
+          secondary: "#f51e26",
+        },
+      },
+    ],
+  },
 };
